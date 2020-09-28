40512b1541fee760f6fcc6104786bd91820bee47
unknown <irankundapatrick01@gmail.com>
2020-09-28 19:26:57 +0200

    setup travis and coveralls

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,7 +1,7 @@
 import sampleFunction from '../index';
 
-// eslint-disable-next-line no-undef
+
 test('adds 1 + 2 to equal 3', () => {
-  // eslint-disable-next-line no-undef
+  
   expect(sampleFunction(1, 2)).toBe(3);
 });
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Nav from './components/Nav';
+import Nav from './components/Nav';
 
-
-// ReactDOM.render(<Nav />, document.getElementById('app'));
+const app = document.createElement('app');
+ReactDOM.render(<Nav />, app);
 
 const sampleFunction = (a, b) => a + b;
 
-
-
 export default sampleFunction;
-
