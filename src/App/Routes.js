5d0321d394025f3218fc929cc3fff15326fde289
Maxime Ishimwe <mxmishimwe5@gmail.com>
2020-11-03 22:53:28 +0200

    fix: added csp rule in index.html meta tag

--- src/App/Routes.js
+++ src/App/Routes.js
@@ -1,20 +1,20 @@
-import React, { useContext } from 'react';
-import { Router, Switch, Route, Redirect } from 'react-router-dom';
-import LandingPage from '../pages/LandingPage/LandingPage';
-import contactPage from '../pages/contactPage/contact';
-import aboutPage from '../pages/aboutPage/aboutPage';
-import Faqs from '../pages/FAQpage/FAQPage';
-import ErrorPage from '../shared/components/ErrorPage/';
+import React, { useContext } from "react";
+import { Router, Switch, Route, Redirect } from "react-router-dom";
+import LandingPage from "../pages/LandingPage/LandingPage";
+import contactPage from "../pages/contactPage/contact";
+import aboutPage from "../pages/aboutPage/aboutPage";
+import Faqs from "../pages/FAQpage/FAQPage";
+import ErrorPage from "../shared/components/ErrorPage/";
 import PrivateRoute from "./PrivateRoute";
 import PrivateLogin from "./PrivateLogin";
-import history from '../browserHistory';
-import AdminPage from 'Dashboard/';
-import AdminLogin from '../pages/admin/loginPage/AdminLogin';
-import { AppContext } from '../context/AppProvider';
+import history from "../browserHistory";
+import AdminPage from "Dashboard/";
+import AdminLogin from "../pages/admin/loginPage/AdminLogin";
+import { AppContext } from "../context/AppProvider";
 
-const Routes = () =>{
-  const {state} = useContext(AppContext);
-  const props = { path: '/admin', component: AdminPage };
+const Routes = () => {
+  const { state } = useContext(AppContext);
+  const props = { path: "/admin", component: AdminPage };
   return (
     <Router history={history}>
       <Switch>
@@ -23,12 +23,20 @@ const Routes = () =>{
         <Route path="/contact" component={contactPage} />
         <Route path="/about" component={aboutPage} />
         <Route path="/faqs" component={Faqs} />
-        <PrivateLogin exact path="/AdminLogin" component={AdminLogin } authenticated={state.currentUser?true:false}/>
-        <PrivateRoute authenticated={state.currentUser?true:false} ownProps={props}/>
+        <PrivateLogin
+          exact
+          path="/AdminLogin"
+          component={AdminLogin}
+          authenticated={state.currentUser ? true : false}
+        />
+        <PrivateRoute
+          authenticated={state.currentUser ? true : false}
+          ownProps={props}
+        />
         <Route component={ErrorPage} />
       </Switch>
     </Router>
   );
-}
+};
 
 export default Routes;
