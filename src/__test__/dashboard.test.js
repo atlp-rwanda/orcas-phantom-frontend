df4e652fec805bd6f807e1e0da49e618b025b525
Ange <ange.ingabire07@gmail.com>
2020-11-25 10:23:04 +0200

    bg-fix-crud-bus-interface(bug fixing the conflicts) the conflicts have been resolved

--- src/__test__/dashboard.test.js
+++ src/__test__/dashboard.test.js
@@ -371,3 +371,47 @@ describe("Tests for Routes section", () => {
     });
   });
 });
+describe("Tests for Bus section", () => {
+  describe("Tests for table sorting and selecting", () => {
+    it(">>>> can select all rows and unselect them", () => {
+      const { getByTestId } = render(<AdminPage />);
+      fireEvent.click(getByTestId("bus"));
+
+      expect(getByTestId("select-all-rows")).toBeValid();
+      fireEvent.click(getByTestId("select-all-rows"));
+
+      expect(getByTestId("select-all-rows")).toBeValid();
+      fireEvent.click(getByTestId("select-all-rows"));
+    });
+  });
+
+  describe("Tests for table pagination", () => {
+    it(">>>> can move to the next and last page", () => {
+      const { getByTestId } = render(<AdminPage />);
+      fireEvent.click(getByTestId("bus"));
+
+      expect(getByTestId("next-page-btn")).toBeValid();
+      expect(getByTestId("last-page-btn")).toBeValid();
+
+      fireEvent.click(getByTestId("next-page-btn"));
+      fireEvent.click(getByTestId("last-page-btn"));
+    });
+
+    it(">>>> can move to the previous and first page", () => {
+      const { getByTestId } = render(<AdminPage />);
+      fireEvent.click(getByTestId("bus"));
+
+      expect(getByTestId("next-page-btn")).toBeValid();
+      expect(getByTestId("last-page-btn")).toBeValid();
+
+      fireEvent.click(getByTestId("next-page-btn"));
+      fireEvent.click(getByTestId("last-page-btn"));
+
+      expect(getByTestId("previous-page-btn")).toBeValid();
+      expect(getByTestId("first-page-btn")).toBeValid();
+
+      fireEvent.click(getByTestId("previous-page-btn"));
+      fireEvent.click(getByTestId("first-page-btn"));
+    });
+  });
+});
